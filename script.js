74a9cb409242178eb6dc4779a0659f0114a651e2
Nihan Samnani <nihansamnani@gmail.com>
2023-12-30 14:56:04 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true
+});
+
+function inprogress(){
+    alert("The webpage is currently undergoing updates and improvements.");
+}
